00e085e4d761ea03802fbd61db279daf70235b0f
Dan Harrin <git@danharrin.com>
2026-02-09 16:53:04 +0000

    Fix spacing in conditional statements

--- entrypoint.php
+++ entrypoint.php
@@ -51,7 +51,7 @@ $branchSwitchedSuccessfully = ($exitCode === 0);
 
 // if the branch doesn't exist we create it and push to origin
 // otherwise we just checkout to the given branch
-if (!$branchSwitchedSuccessfully) {
+if (! $branchSwitchedSuccessfully) {
     note(sprintf('Creating branch "%s" as it doesn\'t exist', $config->getBranch()));
 
     exec_with_output_print(sprintf('git checkout -b %s', $config->getBranch()));
@@ -169,7 +169,7 @@ function list_directory_files(string $directory): void
 function execOrDie(string $command, ?array &$output = []): string|false
 {
     $result = exec($command, $output, $errorCode);
-    if (0 === $errorCode) {
+    if ($errorCode === 0) {
         return $result;
     }
 
